403e48d78c8c06b7d4bbc904bbbc57a35ed6afb5
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-04-26 18:26:04 +0300

    Updates

---
+++ precache-manifest.15c2fc948845ab8288e7fd2256a413ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcb879d5c9e3639ec59e068e9809711",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "dfb3aad66e75e6ea141f",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.3223ec90.chunk.css"
+  },
+  {
+    "revision": "af9729f4fc3226fc20fa",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.19329389.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.19329389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb3aad66e75e6ea141f",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.0555607d.chunk.js"
+  },
+  {
+    "revision": "b7b754233b76014dcb08",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.756f41b0.js"
+  }
+]);
